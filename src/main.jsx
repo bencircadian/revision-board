d6662054fc1d4ccc09404e4a2e4724a2fcc1b7db
bencircadian <ben.circadian@gmail.com>
2025-12-27 23:26:04 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import 'katex/dist/katex.min.css'; // <--- ADD THIS LINE
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
